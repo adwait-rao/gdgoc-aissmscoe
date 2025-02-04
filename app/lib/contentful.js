9142498e01a20da8761d41fe60282b41f9b944e8
Adwait Rao <adwaitrao7000@gmail.com>
2025-02-04 10:55:45 +0530

    fix:env

--- app/lib/contentful.js
+++ app/lib/contentful.js
@@ -1,8 +1,15 @@
 import { createClient } from "contentful";
 
+if (
+  !process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID ||
+  !process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+) {
+  throw new Error("Missing required Contentful environment variables");
+}
+
 const client = createClient({
-  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 });
 
 export default client;
